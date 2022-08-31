2e03c7d208a883b13425360176bdec874ceaaffb
Jack Yeh <jckyeh@gmail.com>
2022-08-31 17:17:29 -0400

    only show requestcaster tags

--- lib/casts.ts
+++ lib/casts.ts
@@ -20,7 +20,7 @@ export async function getCasts() {
       a.body.publishedAt > b.body.publishedAt ? -1 : 1
     );
 
-    return result;
+    return resultRequestCaster;
   } catch (error) {
     console.log(error);
     // return
